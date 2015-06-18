c2863a02580d4854137e46763a235c72ede4aee1
Manuel Kudruss <manuel.kudruss@iwr.uni-heidelberg.de>
2015-06-18 11:48:26 +0200

    finished ad_jcalc and it passes finite difference tests

--- tests/DifferentiationTests.cc
+++ tests/DifferentiationTests.cc
@@ -74,8 +74,29 @@ struct CartPendulum {
 	RigidBodyDynamics::Math::VectorNd qddot;
 	RigidBodyDynamics::Math::VectorNd tau;
 
-
 	Vector3d body_point;
+};
+
+struct ADModel {
+	ADModel () {};
+	void fromModel () {};
+
+	// TODO: Add getter methods with ndirs to check if dimensions fit for functions that write into data structure
+
+	// NOTE: old initialization values
+	//std::vector<MatrixNd> ad_X_lambda_i (ndirs, MatrixNd::Zero (6,6));
+	//std::vector<MatrixNd> ad_X_Ji (ndirs, MatrixNd::Zero (6,6));
+	// std::vector<SpatialVector> ad_V (ndirs, SpatialVector::Zero (6));
+
+	// derivative values
+	// TODO: remove ad_ prefix?
+	std::vector<std::vector<MatrixNd> > ad_X_lambda; // (model.mBodies.size(), ad_X_lambda_i);
+	std::vector<std::vector<MatrixNd> > ad_X_J; // (model->mBodies.size(), ad_X_Ji);
+
+	std::vector<std::vector<SpatialVector> > ad_S;   //(model->mBodies.size(), ad_V);
+	std::vector<std::vector<SpatialVector> > ad_v_J; //(model->mBodies.size(), ad_V);
+	std::vector<std::vector<SpatialVector> > ad_c_J; //(model->mBodies.size(), ad_V);
+
 };
 
 Matrix3d E_from_Matrix(const SpatialMatrix X) {
@@ -96,9 +117,63 @@ Vector3d r_from_Matrix(const SpatialMatrix X) {
 	return r;
 }
 
+Vector3d ad_r_from_Matrix(const SpatialMatrix X, const SpatialMatrix X_dot ) {
+	Matrix3d E_dot = E_from_Matrix(X_dot);
+	Matrix3d E = E_from_Matrix(X);
+
+	Matrix3d Erx_dot = Matrix3d::Zero();
+	Matrix3d Erx = Matrix3d::Zero();
+	Erx_dot = X_dot.block<3,3>(3,0);
+	Erx = X.block<3,3>(3,0);
+
+	Matrix3d rx_dot = E_dot.transpose() * Erx + E.transpose() * Erx_dot;
+	Matrix3d rx = E.transpose() * Erx;
+
+	Vector3d r_dot = Vector3d::Zero();
+	r_dot(0) = -rx_dot(2,1);
+	r_dot(1) =  rx_dot(2,0);
+	r_dot(2) = -rx_dot(1,0);
+
+	return r_dot;
+}
+
+inline SpatialMatrix ad_Xroty (const double &yrot, const double &dot_yrot) {
+	SpatialMatrix result (SpatialMatrix::Zero(6,6));
+
+	double s, c;
+	s = sin (yrot) * dot_yrot;
+	c = cos (yrot) * dot_yrot;
+	Matrix3d E(
+				-s, 0., -c,
+				0., 0., 0.,
+				c, 0., -s
+				);
+
+	result.block<3,3>(0,0) = E;
+	result.block<3,3>(3,3) = E;
+
+	return result;
+}
+
+inline SpatialMatrix ad_Xtrans (const Vector3d &ad_trans) {
+	SpatialMatrix result (SpatialMatrix::Zero(6,6));
+
+	result(3,1) =  ad_trans[2];
+	result(3,2) = -ad_trans[1];
+
+	result(4,0) = -ad_trans[2];
+	result(4,2) =  ad_trans[0];
+
+	result(5,0) =  ad_trans[1];
+	result(5,1) = -ad_trans[0];
+
+	return result;
+}
+
 RBDL_DLLAPI
 void ad_jcalc (
 	Model &model,
+	//ADModel &ad_model,
 	unsigned int joint_id,
 	const VectorNd &q,
 	const MatrixNd &q_dirs,
@@ -110,10 +185,10 @@ void ad_jcalc (
 	std::vector<SpatialVector> &ad_c_Ji
 ) {
 	unsigned int ndirs = q_dirs.cols();
-	cout << "IN: " << __func__ << endl;
-	cout << "ndirs: " << ndirs << endl;
-	cout << "q_dirs.cols(): " << q_dirs.cols() << endl;
-	cout << "qdot_dirs.cols(): " << qdot_dirs.cols() << endl;
+
+	// derivative intermediate values
+	std::vector<MatrixNd> ad_X_lambda_i (ndirs, MatrixNd::Zero (6,6));
+	std::vector<std::vector<MatrixNd> > ad_X_lambda (model.mBodies.size(), ad_X_lambda_i);
 
 	// check input dimensions
 	if (q_dirs.cols() != qdot_dirs.cols()) {
@@ -138,6 +213,66 @@ void ad_jcalc (
 		abort();
 	}
 
+	// exception if we calculate it for the root body
+	assert (joint_id > 0);
+
+	if (model.mJoints[joint_id].mJointType == JointTypeRevoluteX) {
+		std::cerr << "In: " << __func__ << "this joint type is not supported." << std::endl;
+		abort();
+	} else if (model.mJoints[joint_id].mJointType == JointTypeRevoluteY) {
+		// derivative code
+		for (int idir = 0; idir < ndirs; ++idir) {
+			ad_X_Ji[idir] = ad_Xroty (q[model.mJoints[joint_id].q_index], q_dirs(model.mJoints[joint_id].q_index, idir));
+			ad_S_i[idir]  = SpatialVector::Zero(); // S = [0., 1., 0., 0., 0., 0.]
+			ad_v_Ji[idir][1] = qdot_dirs(model.mJoints[joint_id].q_index, idir); // v_J = S*qdot
+			ad_c_Ji[idir] = SpatialVector::Zero(); // c_J = Sdot*qdot
+		}
+		// nominal code
+		model.X_J[joint_id] = Xroty (q[model.mJoints[joint_id].q_index]);
+		model.v_J[joint_id][1] = qdot[model.mJoints[joint_id].q_index];
+	} else if (model.mJoints[joint_id].mJointType == JointTypeRevoluteZ) {
+		std::cerr << "In: " << __func__ << "this joint type is not supported." << std::endl;
+		abort();
+	} else if (model.S[joint_id] == SpatialVector (0., 0., 0., 1., 0., 0.)) {
+		// derivative code
+		for (int idir = 0; idir < ndirs; ++idir) {
+			ad_X_Ji[idir] = ad_Xtrans (Vector3d (q_dirs(model.mJoints[joint_id].q_index, idir), 0.0, 0.0));
+			ad_S_i[idir]  = SpatialVector::Zero(); // S = [0., 0., 0., 1., 0., 0.]
+			ad_v_Ji[idir][3] = qdot_dirs(model.mJoints[joint_id].q_index, idir); // v_J = S*qdot
+			ad_c_Ji[idir] = SpatialVector::Zero(); // v_J = Sdot*qdot
+		}
+		// nominal code
+		model.X_J[joint_id] = Xtrans (Vector3d (q[model.mJoints[joint_id].q_index], 0., 0.));
+		model.v_J[joint_id][3] = qdot[model.mJoints[joint_id].q_index];
+	} else if (model.mJoints[joint_id].mDoFCount == 1) {
+		std::cerr << "In: " << __func__ << "this joint type is not supported." << std::endl;
+		abort();
+	} else if (model.mJoints[joint_id].mJointType == JointTypeSpherical) {
+		std::cerr << "In: " << __func__ << "this joint type is not supported." << std::endl;
+		abort();
+	} else if (model.mJoints[joint_id].mJointType == JointTypeEulerZYX) {
+		std::cerr << "In: " << __func__ << "this joint type is not supported." << std::endl;
+		abort();
+	} else if (model.mJoints[joint_id].mJointType == JointTypeEulerXYZ) {
+		std::cerr << "In: " << __func__ << "this joint type is not supported." << std::endl;
+		abort();
+	} else if (model.mJoints[joint_id].mJointType == JointTypeEulerYXZ) {
+		std::cerr << "In: " << __func__ << "this joint type is not supported." << std::endl;
+		abort();
+	} else if (model.mJoints[joint_id].mJointType == JointTypeTranslationXYZ) {
+		std::cerr << "In: " << __func__ << "this joint type is not supported." << std::endl;
+		abort();
+	} else {
+		std::cerr << "Error: invalid joint type " << model.mJoints[joint_id].mJointType << " at id " << joint_id << std::endl;
+		abort();
+	}
+	// derivative code
+	for (int idir = 0; idir < ndirs; ++idir) {
+		// TODO: we need ad_X_lambda in ad data structures
+		ad_X_lambda[joint_id][idir] = ad_X_Ji[idir] * model.X_T[joint_id].toMatrix();
+	}
+	// nominal code
+	model.X_lambda[joint_id] = model.X_J[joint_id] * model.X_T[joint_id];
 };
 
 
@@ -178,61 +313,8 @@ Vector3d CalcBodyToBaseCoordinatesSingleFunc (
 	return body_position + body_rotation.transpose() * point_body_coordinates;
 }
 
-inline SpatialMatrix dq_Xroty (const double &yrot, const double &dot_yrot) {
-	SpatialMatrix result (SpatialMatrix::Zero(6,6));
-
-	double s, c;
-	s = sin (yrot) * dot_yrot;
-	c = cos (yrot) * dot_yrot;
-	Matrix3d E(
-				-s, 0., -c,
-				0., 0., 0.,
-				c, 0., -s
-				);
-
-	result.block<3,3>(0,0) = E;
-	result.block<3,3>(3,3) = E;
-
-	return result;
-}
-
-inline SpatialMatrix dq_Xtrans (const Vector3d &dq_trans) {
-	SpatialMatrix result (SpatialMatrix::Zero(6,6));
-
-	result(3,1) =  dq_trans[2];
-	result(3,2) = -dq_trans[1];
-
-	result(4,0) = -dq_trans[2];
-	result(4,2) =  dq_trans[0];
-
-	result(5,0) =  dq_trans[1];
-	result(5,1) = -dq_trans[0];
-
-	return result;
-}
-
-Vector3d dq_r_from_Matrix(const SpatialMatrix X, const SpatialMatrix X_dot ) {
-	Matrix3d E_dot = E_from_Matrix(X_dot);
-	Matrix3d E = E_from_Matrix(X);
-
-	Matrix3d Erx_dot = Matrix3d::Zero();
-	Matrix3d Erx = Matrix3d::Zero();
-	Erx_dot = X_dot.block<3,3>(3,0);
-	Erx = X.block<3,3>(3,0);
-
-	Matrix3d rx_dot = E_dot.transpose() * Erx + E.transpose() * Erx_dot;
-	Matrix3d rx = E.transpose() * Erx;
-
-	Vector3d r_dot = Vector3d::Zero();
-	r_dot(0) = -rx_dot(2,1);
-	r_dot(1) =  rx_dot(2,0);
-	r_dot(2) = -rx_dot(1,0);
-
-	return r_dot;
-}
-
 RBDL_DLLAPI
-Vector3d dq_CalcBodyToBaseCoordinatesSingleFunc (
+Vector3d ad_CalcBodyToBaseCoordinatesSingleFunc (
 		Model &model,
 		const VectorNd &q,
 		const MatrixNd &q_dirs,
@@ -252,32 +334,32 @@ Vector3d dq_CalcBodyToBaseCoordinatesSingleFunc (
 	VectorNd QDot_zero (VectorNd::Zero (model.q_size));
 	VectorNd fd_out (MatrixNd::Zero (3, model.q_size));
 
-	std::vector<MatrixNd> dq_X_J_i (ndirs, MatrixNd::Zero (6,6));
-	std::vector<std::vector<MatrixNd> > dq_X_J (model.mBodies.size(), dq_X_J_i);
-	std::vector<std::vector<MatrixNd> > fd_X_J (model.mBodies.size(), dq_X_J_i);
-	// dq_X_J[3][5] gives for body 3 the 5th direction
+	std::vector<MatrixNd> ad_X_J_i (ndirs, MatrixNd::Zero (6,6));
+	std::vector<std::vector<MatrixNd> > ad_X_J (model.mBodies.size(), ad_X_J_i);
+	std::vector<std::vector<MatrixNd> > fd_X_J (model.mBodies.size(), ad_X_J_i);
+	// ad_X_J[3][5] gives for body 3 the 5th direction
 
-	std::vector<MatrixNd> dq_X_lambda_i (ndirs, MatrixNd::Zero (6,6));
-	std::vector<std::vector<MatrixNd> > dq_X_lambda (model.mBodies.size(), dq_X_lambda_i);
-	std::vector<std::vector<MatrixNd> > fd_X_lambda (model.mBodies.size(), dq_X_lambda_i);
-	// dq_X_lambda[3][5] gives for body 3 the 5th direction
+	std::vector<MatrixNd> ad_X_lambda_i (ndirs, MatrixNd::Zero (6,6));
+	std::vector<std::vector<MatrixNd> > ad_X_lambda (model.mBodies.size(), ad_X_lambda_i);
+	std::vector<std::vector<MatrixNd> > fd_X_lambda (model.mBodies.size(), ad_X_lambda_i);
+	// ad_X_lambda[3][5] gives for body 3 the 5th direction
 
-	std::vector<MatrixNd> dq_X_base_i (ndirs, MatrixNd::Zero (6,6));
-	std::vector<std::vector<MatrixNd> > dq_X_base (model.mBodies.size(), dq_X_base_i);
-	std::vector<std::vector<MatrixNd> > fd_X_base (model.mBodies.size(), dq_X_base_i);
-	// dq_X_base[3][5] gives for body 3 the 5th direction
+	std::vector<MatrixNd> ad_X_base_i (ndirs, MatrixNd::Zero (6,6));
+	std::vector<std::vector<MatrixNd> > ad_X_base (model.mBodies.size(), ad_X_base_i);
+	std::vector<std::vector<MatrixNd> > fd_X_base (model.mBodies.size(), ad_X_base_i);
+	// ad_X_base[3][5] gives for body 3 the 5th direction
 
 	for (unsigned int i = 1; i < model.mBodies.size(); i++) {
 		unsigned int lambda = model.lambda[i];
 		// Calculate joint dependent variables
 		if (model.mJoints[i].mJointType == JointTypeRevoluteY) {
 			for (unsigned int j = 0; j < ndirs; j++) {
-				dq_X_J[i][j] = dq_Xroty (q[model.mJoints[i].q_index], q_dirs(i-1,j));
+				ad_X_J[i][j] = ad_Xroty (q[model.mJoints[i].q_index], q_dirs(i-1,j));
 			}
 			model.X_J[i] = Xroty (q[model.mJoints[i].q_index]);
 		} else if (model.S[i] == SpatialVector (0., 0., 0., 1., 0., 0.)) {
 			for (unsigned int j = 0; j < ndirs; j++) {
-				dq_X_J[i][j] = dq_Xtrans (Vector3d (q_dirs(i-1, j), 0., 0.));
+				ad_X_J[i][j] = ad_Xtrans (Vector3d (q_dirs(i-1, j), 0., 0.));
 			}
 			model.X_J[i] = Xtrans (Vector3d (1., 0., 0.) * q[model.mJoints[i].q_index]);
 		} else {
@@ -286,22 +368,22 @@ Vector3d dq_CalcBodyToBaseCoordinatesSingleFunc (
 		}
 
 		for (unsigned int j = 0; j < ndirs; j++) {
-			dq_X_lambda[i][j] = dq_X_J[i][j] * model.X_T[i].toMatrix();
+			ad_X_lambda[i][j] = ad_X_J[i][j] * model.X_T[i].toMatrix();
 		}
 		model.X_lambda[i] = model.X_J[i] * model.X_T[i];
 
 		for (unsigned int j = 0; j < ndirs; j++) {
-			dq_X_base[i][j] = dq_X_lambda[i][j] * model.X_base[lambda].toMatrix() + model.X_lambda[i].toMatrix() * dq_X_base[lambda][j];
+			ad_X_base[i][j] = ad_X_lambda[i][j] * model.X_base[lambda].toMatrix() + model.X_lambda[i].toMatrix() * ad_X_base[lambda][j];
 		}
 		model.X_base[i] = model.X_lambda[i] * model.X_base[lambda];
 	}
 
 	for (unsigned int j = 0; j < ndirs; j++) {
 		SpatialMatrix X_base_ib = model.X_base[body_id].toMatrix();
-		Matrix3d dq_E = E_from_Matrix(dq_X_base[body_id][j]);
-		Vector3d dq_r = dq_r_from_Matrix(X_base_ib, dq_X_base[body_id][j]);
+		Matrix3d ad_E = E_from_Matrix(ad_X_base[body_id][j]);
+		Vector3d ad_r = ad_r_from_Matrix(X_base_ib, ad_X_base[body_id][j]);
 
-		out.block<3,1>(0,j) = dq_r + dq_E.transpose() * point_body_coordinates;
+		out.block<3,1>(0,j) = ad_r + ad_E.transpose() * point_body_coordinates;
 	}
 
 	Matrix3d body_rotation = E_from_Matrix(model.X_base[body_id].toMatrix());
@@ -391,7 +473,7 @@ Vector3d fd_dq_CalcBodyToBaseCoordinatesSingleFunc (
 		Vector3d res_hd = CalcBodyToBaseCoordinatesSingleFunc (model, q + h * q_dir, body_id, point_body_coordinates);
 		Vector3d res_hd_rbdl = CalcBodyToBaseCoordinates (model, q + h * q_dir, body_id, point_body_coordinates);
 
-		cout << "calc_body err = " << (res_hd - res_hd_rbdl).transpose() << endl;
+		//cout << "calc_body err = " << (res_hd - res_hd_rbdl).transpose() << endl;
 
 		out.block<3,1>(0,j) = (res_hd - ref) / h;
 	}
@@ -400,6 +482,8 @@ Vector3d fd_dq_CalcBodyToBaseCoordinatesSingleFunc (
 }
 
 TEST_FIXTURE (CartPendulum, jcalcNominalSolutionTest) {
+	cout << "Nominal Evaluation Test" << endl;
+
 	// set nominal values
 	q.setZero();
 	q[0] = 0.3;
@@ -411,7 +495,6 @@ TEST_FIXTURE (CartPendulum, jcalcNominalSolutionTest) {
 
 	// set directions
 	unsigned int ndirs = model->q_size + model->qdot_size;
-	cout << "ndirs: " << ndirs << endl;
 	MatrixNd x = MatrixNd::Identity(ndirs, ndirs);
 	MatrixNd q_dirs = x.block(0, 0, model->q_size, ndirs);
 	MatrixNd qdot_dirs = x.block(model->q_size, 0, model->qdot_size, ndirs);
@@ -445,25 +528,36 @@ TEST_FIXTURE (CartPendulum, jcalcNominalSolutionTest) {
 		CHECK_ARRAY_CLOSE (ref_S_i.data(),  test_S_i.data(),   6, TEST_PREC);
 		CHECK_ARRAY_CLOSE (ref_v_Ji.data(), test_v_Ji.data(),  6, TEST_PREC);
 		CHECK_ARRAY_CLOSE (ref_c_Ji.data(), test_c_Ji.data(),  6, TEST_PREC);
+		/* DEBUG OUTPUT
+		cout << "===== joint_id: " << joint_id << " =====" << endl;
 		cout << "ref_X_Ji: " << endl << ref_X_Ji << endl;
 		cout << "test_X_Ji: " << endl << test_X_Ji << endl;
 		cout << "error_X_Ji: " << endl << ref_X_Ji - test_X_Ji << endl;
+		cout << endl;
 
 		cout << "ref_S_i: " << endl << ref_S_i << endl;
 		cout << "test_S_i: " << endl << test_S_i << endl;
 		cout << "error_S_i: " << endl << ref_S_i - test_S_i << endl;
+		cout << endl;
 
 		cout << "ref_v_Ji: " << endl << ref_v_Ji << endl;
 		cout << "test_v_Ji: " << endl << test_v_Ji << endl;
 		cout << "error_v_Ji: " << endl << ref_v_Ji - test_v_Ji << endl;
+		cout << endl;
 
 		cout << "ref_c_Ji: " << endl << ref_c_Ji << endl;
 		cout << "test_c_Ji: " << endl << test_c_Ji << endl;
 		cout << "error_c_Ji: " << endl << ref_c_Ji - test_c_Ji << endl;
+		cout << endl;
+		cout << endl;
+		*/
 	}
 }
 
 TEST_FIXTURE (CartPendulum, jcalcFDvsADTest) {
+	cout << "Derivative Evaluation Test" << endl;
+	double TEST_PREC = 1.0e-08;
+
 	// set nominal values
 	q.setZero();
 	q[0] = 0.3;
@@ -507,6 +601,8 @@ TEST_FIXTURE (CartPendulum, jcalcFDvsADTest) {
 			CHECK_ARRAY_CLOSE (fd_S[joint_id][idir].data(),   ad_S[joint_id][idir].data(),    6, TEST_PREC);
 			CHECK_ARRAY_CLOSE (fd_v_J[joint_id][idir].data(), ad_v_J[joint_id][idir].data(),  6, TEST_PREC);
 			CHECK_ARRAY_CLOSE (fd_c_J[joint_id][idir].data(), ad_c_J[joint_id][idir].data(),  6, TEST_PREC);
+			/* DEBUG OUTPUT
+			cout << "===== joint_id: " << joint_id << ", idir: " << idir << " =====" << endl;
 			cout << "fd_X_J[" << joint_id << "][" << idir << "]: " << endl << fd_X_J[joint_id][idir] << endl;
 			cout << "ad_X_J[" << joint_id << "][" << idir << "]: " << endl << ad_X_J[joint_id][idir] << endl;
 			cout << "error_X_J: " << endl << fd_X_J[joint_id][idir] - ad_X_J[joint_id][idir] << endl;
@@ -522,6 +618,7 @@ TEST_FIXTURE (CartPendulum, jcalcFDvsADTest) {
 			cout << "fd_c_J[" << joint_id << "][" << idir << "]: " << endl << fd_c_J[joint_id][idir] << endl;
 			cout << "ad_c_J[" << joint_id << "][" << idir << "]: " << endl << ad_c_J[joint_id][idir] << endl;
 			cout << "error_c_J: " << endl << fd_c_J[joint_id][idir] - ad_c_J[joint_id][idir] << endl;
+			*/
 		}
 	}
 }
@@ -551,7 +648,7 @@ TEST_FIXTURE ( CartPendulum, CartPendulumJacobianADSimple ) {
 
 	MatrixNd q_dirs = MatrixNd::Identity (model->qdot_size, model->qdot_size);
 	Vector3d base_point_standard = CalcBodyToBaseCoordinates (*model, q, id_pendulum, body_point);
-	Vector3d base_point_ad = dq_CalcBodyToBaseCoordinatesSingleFunc (*model, q, q_dirs, id_pendulum, body_point, jacobian_ad);
+	Vector3d base_point_ad = ad_CalcBodyToBaseCoordinatesSingleFunc (*model, q, q_dirs, id_pendulum, body_point, jacobian_ad);
 	Vector3d base_point_fd = fd_dq_CalcBodyToBaseCoordinatesSingleFunc (*model, q, q_dirs, id_pendulum, body_point, jacobian_fd);
 
 	CHECK_ARRAY_CLOSE (jacobian_ref.data(), jacobian_ad.data(), 3 * model->qdot_size, TEST_PREC);
